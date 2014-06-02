b201ccafb84f788c3baa25907c376a6bbaaf3cfc
Kyoshim <legolas2_chris@hotmail.fr>
2014-06-02 13:01:17 +0200

    bootstrap + less

--- wp-content/themes/manga_world/header.php
+++ wp-content/themes/manga_world/header.php
@@ -3,18 +3,17 @@
 <head>
     <meta charset="utf-8">
     <title>Manga World</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.less">
+    <!-- Optional theme -->
+    <link rel="<?php echo get_template_directory_uri(); ?>/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/less-1.7.0.min.js" type="text/javascript"></script>
 </head>
 <body>
 <header>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- Optional theme -->
-    <link rel="css/bootstrap-theme.min.css">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="css/bootstrap.min.js"></script>
-
     <?php if (is_single()) : ?>
         <p><a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a></p>
         <p><?php bloginfo('description'); ?></p>
