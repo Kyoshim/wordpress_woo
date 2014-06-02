06361c9590f541240bbdb75b3ea963808b59c8e8
egonay <Elo@Elo.local>
2014-06-02 12:51:18 +0200

    Merge branch 'master' of https://github.com/Kyoshim/wordpress_woo
    Conflicts:
    	.idea/workspace.xml
    	wp-content/themes/manga_world/functions.php
    	wp-content/themes/manga_world/header.php
    	wp-content/themes/manga_world/sidebar.php

---
+++ wp-content/themes/manga_world/functions.php
@@ -0,0 +1,16 @@
+<?php
+add_theme_support( 'post-thumbnails' );
+<<<<<<< HEAD
+=======
+
+if ( function_exists('register_sidebar') )
+    register_sidebar();
+
+if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="section">',
+        'after_title' => '</h4>',
+    ));
+>>>>>>> FETCH_HEAD
