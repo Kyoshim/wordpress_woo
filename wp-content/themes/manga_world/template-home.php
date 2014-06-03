459e4f892af6e7456140a37235c4d9617af6b7f0
Kyoshim <legolas2_chris@hotmail.fr>
2014-06-04 00:45:56 +0200

    home_MAJ

--- wp-content/themes/manga_world/template-home.php
+++ wp-content/themes/manga_world/template-home.php
@@ -2,20 +2,32 @@
 /*
 Template Name: home
 */
-
-
 ?>
 
 
 <?php get_header(); ?>
 
-<div class="nouveautes">
-    <h3><?php the_field('title_nouveautes'); ?></h3>
+<div class="content">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="nouveautes block_content">
+                <h3><?php the_field('title_nouveautes'); ?></h3>
+            </div>
 
+            <div class="dvd block_content">
+                <h3><?php the_field('title_dvd'); ?></h3>
+            </div>
 
+            <div class="blu_ray block_content">
+                <h3><?php the_field('title_blu_ray'); ?></h3>
+            </div>
+
+            <div class="figurines block_content">
+                <h3><?php the_field('title_figurines'); ?></h3>
+            </div>
+        </div>
+    </div>
 </div>
-
-<p><?php the_field('title', 12); ?></p>
 
 <img src="<?php the_field('logo'); ?>" alt=""/>
 
