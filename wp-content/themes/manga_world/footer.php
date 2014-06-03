c368eb1673deca409572c090b39c766fe29b16f4
Sohuth <badmutafukaz@gmail.com>
2014-06-03 12:35:50 +0200

    Thème Structure

--- wp-content/themes/manga_world/footer.php
+++ wp-content/themes/manga_world/footer.php
@@ -1,6 +1,73 @@
 <footer>
-    <?php bloginfo('name'); ?> est propulsé par <a href="http://wordpress.org">WordPress</a>.
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="widget of_contact_widget"><h5 class="widgetheading">Contact info</h5>		<address>
+                            <strong>Moderna company Inc </strong><br>
+
+                            Modernbuilding suite V124, AB 01 Someplace 16425 Earth 				</address>
+                        <p>
+                            <i class="icon-phone"></i>(123) 456-7890 - (123) 555-7891<br>
+
+                            <i class="icon-envelope-alt"></i><a href="mailto:email@domainname.com">email@domainname.com</a>
+                        </p>
+
+
+                    </div>			</div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="widget widget_pages"><h5 class="widgetheading">Pages</h5>		<ul>
+                            <li class="page_item page-item-49"><a href="http://bootstraptaste.com/wp/moderna/blog/">Blog</a></li>
+                            <li class="page_item page-item-64"><a href="http://bootstraptaste.com/wp/moderna/components/">Components</a></li>
+                            <li class="page_item page-item-51"><a href="http://bootstraptaste.com/wp/moderna/contact/">Contact</a></li>
+                            <li class="page_item page-item-5 current_page_item"><a href="http://bootstraptaste.com/wp/moderna/">Home</a></li>
+                            <li class="page_item page-item-45"><a href="http://bootstraptaste.com/wp/moderna/portfolio/">Portfolio</a></li>
+                            <li class="page_item page-item-56"><a href="http://bootstraptaste.com/wp/moderna/pricing-box/">Pricing box</a></li>
+                        </ul>
+                    </div>			</div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="widget widget_recent_entries">		<h5 class="widgetheading">Recent Posts</h5>		<ul>
+                            <li>
+                                <a href="http://bootstraptaste.com/wp/moderna/cu-eos-sale-sanctus-eligendi/">Cu eos sale sanctus eligendi</a>
+                            </li>
+                            <li>
+                                <a href="http://bootstraptaste.com/wp/moderna/volumus-intellegebat-has-exper-in/">Volumus intellegebat has exper in</a>
+                            </li>
+                            <li>
+                                <a href="http://bootstraptaste.com/wp/moderna/hello-world/">Lorem ipsum dolor sit amet</a>
+                            </li>
+                        </ul>
+                    </div>			</div>
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="widget iweb_flickr_widget"><h5 class="widgetheading">Flickr Photos</h5>	<div class="flickr_badge">
+                            <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=91212552@N07"></script><div class="flickr_badge_image" id="flickr_badge_image1"><a href="http://www.flickr.com/photos/91212552@N07/8284272962/"><img src="http://farm9.staticflickr.com/8345/8284272962_9acc1cdddc_s.jpg" alt="Une photo sur Flickr" title="Squirrel" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image2"><a href="http://www.flickr.com/photos/91212552@N07/8284244308/"><img src="http://farm9.staticflickr.com/8357/8284244308_9b4fc51307_s.jpg" alt="Une photo sur Flickr" title="Speed )" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image3"><a href="http://www.flickr.com/photos/91212552@N07/8284251058/"><img src="http://farm9.staticflickr.com/8078/8284251058_be90b62fdc_s.jpg" alt="Une photo sur Flickr" title="Blue sky" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image4"><a href="http://www.flickr.com/photos/91212552@N07/8283213165/"><img src="http://farm9.staticflickr.com/8490/8283213165_913047d552_s.jpg" alt="Une photo sur Flickr" title="Squirrel 4" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image5"><a href="http://www.flickr.com/photos/91212552@N07/8284272860/"><img src="http://farm9.staticflickr.com/8342/8284272860_e664e7a1d0_s.jpg" alt="Une photo sur Flickr" title="Squirrel 3" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image6"><a href="http://www.flickr.com/photos/91212552@N07/8284272900/"><img src="http://farm9.staticflickr.com/8348/8284272900_3d86d8ed10_s.jpg" alt="Une photo sur Flickr" title="Squirrel 2" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image7"><a href="http://www.flickr.com/photos/91212552@N07/8283192651/"><img src="http://farm9.staticflickr.com/8348/8283192651_5555855060_s.jpg" alt="Une photo sur Flickr" title="Car" height="75" width="75"></a></div><div class="flickr_badge_image" id="flickr_badge_image8"><a href="http://www.flickr.com/photos/91212552@N07/8284250826/"><img src="http://farm9.staticflickr.com/8197/8284250826_220b607fe3_s.jpg" alt="Une photo sur Flickr" title="On the road" height="75" width="75"></a></div><span style="position:absolute;left:-999em;top:-999em;visibility:hidden" class="flickr_badge_beacon"><img src="http://geo.yahoo.com/p?s=792600102&amp;t=e29768faae2c154885905dca71197124&amp;r=http%3A%2F%2Fbootstraptaste.com%2Fwp%2Fmoderna%2F&amp;fl_ev=0&amp;lang=fr&amp;intl=us" width="0" height="0" alt="" style="display: none !important;"></span>
+                        </div>
+
+                    </div>			</div>
+            </div>
+        </div>
+        <div id="sub-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="copyright">
+                            <p>
+                                <span>© Moderna 2014 All right reserved. By Bootstraptaste</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+
+                        <ul class="social-network">
+                            <li><a data-placement="top" title="" target="_blank" href="https://facebook.com/username" data-original-title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a data-placement="top" target="_blank" title="" href="https://twitter.com/" data-original-title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a data-placement="top" target="_blank" title="" href="https://linkedin.com/" data-original-title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a data-placement="top" target="_blank" title="" href="https://pinterest.com/" data-original-title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a data-placement="top" target="_blank" title="" href="https://google.com" data-original-title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 </footer>
 <?php wp_footer(); ?>
-</body>
-</html>
